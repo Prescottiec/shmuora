f791875e1edb201845c93eefb2056f1a870a31a0
Prescott <carlsonprescott@gmail.com>
2021-08-08 17:38:42 -0700

    user auth debug

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 
 import GreetingContainer from "./greeting/greeting_container";
-// import SignUpFormContainer from "./session_form/signup_form_container";
+import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
 
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
@@ -17,8 +17,8 @@ const App = () => (
       <GreetingContainer />
     </header>
     <Switch>
-      <AuthRoute exact path="/" component={LogInFormContainer} />
-      {/* <AuthRoute exact path="/" component={SignUpFormContainer} /> */}
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
     </Switch>
   </div>
 );
